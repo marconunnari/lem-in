99db4864b513e455bb0ca996d17cb33102d02336
Marco Nunnari <marnunna@gmail.com>
2017-06-15 01:41:58 +0200

    parse ants

--- srcs/main.c
+++ srcs/main.c
@@ -12,7 +12,37 @@
 
 #include "lem_in.h"
 
-int			main()
+void		li_error(int cond, char *msg)
 {
-	ft_printf("hello lem-in\n");
+	if (cond)
+		ft_error(msg, 1);
+}
+
+uintmax_t	parse_ants()
+{
+	int	i;
+	char	*line;
+
+	i = get_next_line(0, &line);
+	li_error(i == -1, "input error");
+	i = 0;
+	while (line[i])
+	{
+		li_error(!ft_isdigit(line[i]), "number of ants not numeric");
+		i++;
+	}
+	li_error(i > 20, "number of ants too big");
+	if (i == 20)
+		li_error(ft_strcmp(line,"18446744073709551615") > 0, "number of ants too big");
+	return (ft_atouimax(line));
+}
+
+int			main(int argc, char **argv)
+{
+	uintmax_t	ants;
+
+	(void)argv;
+	li_error(argc > 1, "too many arguments");
+	ants = parse_ants();
+	ft_printfnl("ants: %ju", ants);
 }
