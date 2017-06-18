27e42653ea79c71a727bc7c97641b0467da444fc
Marco Nunnari <marnunna@gmail.com>
2017-06-18 17:32:31 +0200

    minilibx 2.0

--- visu-hex/includes/visu_hex.h
+++ visu-hex/includes/visu_hex.h
@@ -14,7 +14,7 @@
 # define VISU_HEX_H
 
 # include "lem_in.h"
-# include "mlx.h"
+# include "../minilibx/includes/mlx.h"
 # include <pthread.h>
 
 # define SW 800
@@ -24,29 +24,6 @@ typedef	struct	s_move
 	uintmax_t		ant;
 	t_room			*dest;
 }				t_move;
-
-typedef	struct	s_point
-{
-	int			x;
-	int			y;
-}				t_point;
-
-typedef	struct	s_rect
-{
-	int			swap;
-	int			dx;
-	int			dy;
-	int			ix;
-	int			iy;
-}				t_rect;
-
-typedef	struct	s_image
-{
-	char		*img;
-	int			bpp;
-	int			linesize;
-	int			endian;
-}				t_image;
 
 typedef	struct	s_hook_moves
 {
@@ -59,9 +36,6 @@ typedef	struct	s_hook_moves
 	t_list		*turns;
 }				t_hook_moves;
 
-void		fill_square(t_image image, t_point p1, size_t size, int color);
-void		fill_rect(t_image image, t_point p1, t_point p2, int color);
-void		fill_pixel(t_image img, int x, int y, int color);
 void		draw_room(t_image image, t_li_info *li_info, t_room *room);
 void		draw_links(t_image image, t_hex *hex);
 void		draw_rooms(t_image image, t_hex *hex, t_li_info *li_info);
